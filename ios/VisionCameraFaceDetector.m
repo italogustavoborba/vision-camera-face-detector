3ea697a23d3f095efd76fa60f07b14309877d36b
Italo Teixeira <italogustavoborba@gmail.com>
2023-12-13 15:15:58 -0300

    wip

--- ios/VisionCameraFaceDetector.m
+++ ios/VisionCameraFaceDetector.m
@@ -1,5 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <VisionCamera/FrameProcessorPlugin.h>
+#import <VisionCamera/FrameProcessorPluginRegistry.h>
+
+#import "VisionCameraFaceDetector.h"
 
 @interface VISION_EXPORT_SWIFT_FRAME_PROCESSOR(scanFaces, VisionCameraFaceDetector)
 @end
